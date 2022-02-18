0a5d217565d17fa71405681a68aca46bdcd313ef
spaceman1412 <spacemannormal@gmail.com>
2022-02-18 11:07:52 +0700

    Add the keyboard view and clear text after add

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.todolist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,27 +18,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        var TodoList = mutableListOf(
-            Todo("Hello"),
-            Todo("Hello"),
-            Todo("Hello"),
+        var TodoList = mutableListOf<Todo>(
+
         )
 
         val adapter = TodoAdapter(TodoList)
-        val rvView : RecyclerView = findViewById(R.id.rvView)
+        val rvView: RecyclerView = findViewById(R.id.rvView)
 
         rvView.adapter = adapter
         rvView.layoutManager = LinearLayoutManager(this)
 
-        val btnAdd : Button = findViewById(R.id.btnAdd)
-        val editText : EditText = findViewById(R.id.editTxt)
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+        val editText: EditText = findViewById(R.id.editTxt)
 
-        btnAdd.setOnClickListener{
-            val title = editText.text.toString()
-            val todo = Todo(title)
-            TodoList.add(todo)
-            adapter.notifyItemInserted(TodoList.size -1)
+
+        btnAdd.setOnClickListener {
+            if (!checkNull(editText)) {
+                val title = editText.text.toString()
+                val todo = Todo(title)
+                TodoList.add(todo)
+                adapter.notifyItemInserted(TodoList.size - 1)
+                val inputManager: InputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputManager.hideSoftInputFromWindow(
+                    currentFocus?.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+                editText.text.clear()
+            } else {
+                Toast.makeText(this@MainActivity, "Please input the note!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
         }
 
+
+    }
+
+    private fun checkNull(editText: EditText): Boolean {
+        return editText.text.toString() == ""
     }
 }
